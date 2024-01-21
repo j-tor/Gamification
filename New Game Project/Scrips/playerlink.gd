de1d7d91a30ad181f7849f2bd90b595e5c7fd8b2
Jeff <124184325+j-tor@users.noreply.github.com>
2024-01-20 21:29:36 -0700

    88
    jj

---
+++ New Game Project/Scrips/playerlink.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var speed = 100
+
+@onready var animations = $AnimationPlayer
+
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+	
+func updateAnimation():
+	
+	var direction = "Walk"
+	if velocity.x < 0 : direction = "Left"
+	elif velocity.x > 0 : direction = "Right"
+	elif velocity.y < 0 : direction = "Up"
+	elif velocity.y > 0 : direction = "Down"
+	
+	
+	animations.play("run"+direction)
+	
+	
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
+	updateAnimation()
