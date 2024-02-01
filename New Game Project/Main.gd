9b3cf7c6f5131f38c320926729e4e955361c6730
Jeff <124184325+j-tor@users.noreply.github.com>
2024-01-31 23:27:22 -0700

    Update  avance minigame
    se actualizo los script y agrego la ruleta(sprite)

--- New Game Project/Main.gd
+++ New Game Project/Main.gd
@@ -1,8 +1,5 @@
-extends Node2D
+extends Node
 
-
-
-#batallas
 var ataque_jugador = false
 var objetivo_enemigo = 1
 var objetivo_player  = 1
@@ -14,11 +11,23 @@ var nombre_objeto
 var dialogo_objeto = []
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
 func _dialogo():
 	match nombre_objeto:
 		"letrero":
 			dialogo_objeto = [
 				"Esta cerrado. . . . . . . . . .",#0
-				"Talvez tengo  que cumplir una mision antes",#1
+				"Tinnes que cumplir con la mision de la ruleta antes",#1
 				"Regresare despues"#2
 			]
--- New Game Project/Scrips/playerlink.gd
+++ New Game Project/Scrips/playerlink.gd
@@ -3,18 +3,32 @@ extends CharacterBody2D
 var speed =280
 
 @onready var animations = $AnimationPlayer
+var mover = true
+var objeto = false
 
 func get_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
+	if mover == true:
+		if objeto == true:
+			if Input.is_action_just_pressed("tecla_f"):
+				mover = false
 	
 func updateAnimation():
 	
 	var direction = "Walk"
-	if velocity.x < 0 : direction = "Left"
-	elif velocity.x > 0 : direction = "Right"
-	elif velocity.y < 0 : direction = "Up"
-	elif velocity.y > 0 : direction = "Down"
+	if velocity.x < 0 : 
+		direction = "Left" 
+		$Detec_obj/CollisionShape2D.rotation_degrees =90
+	elif velocity.x > 0 : 
+		direction = "Right"
+		$Detec_obj/CollisionShape2D.rotation_degrees =270
+	elif velocity.y < 0 : 
+		direction = "Up"
+		$Detec_obj/CollisionShape2D.rotation_degrees =180
+	elif velocity.y > 0 : 
+		direction = "Down"
+		$Detec_obj/CollisionShape2D.rotation_degrees =0
 	else:animations.stop()
 
 	animations.play("run"+direction)
@@ -26,6 +40,7 @@ func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
 	updateAnimation()
+	
 
 
 func _on_regreso_area_entered(area):
@@ -34,3 +49,14 @@ func _on_regreso_area_entered(area):
 		get_tree().get_nodes_in_group("Game")[0].ubicacion = Vector2(86,-938)
 		get_tree().get_nodes_in_group("Game")[0]._verificar_nivel()
 		get_tree().get_nodes_in_group("casa")[0].queue_free()
+
+
+func _on_detec_obj_area_entered(area):
+	if area.is_in_grup("objeto"):
+		objeto = true
+	
+
+
+func _on_detec_obj_area_exited(area):
+	if area.is_in_grup("objeto"):
+		objeto = false
