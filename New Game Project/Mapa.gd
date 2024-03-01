aa830a73c33ad2c91016cf038118429021e72407
Jeff <124184325+j-tor@users.noreply.github.com>
2024-03-01 04:10:45 -0700

    all buff
    done all buff

--- New Game Project/Mapa.gd
+++ New Game Project/Mapa.gd
@@ -4,12 +4,15 @@ extends TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Main.Vida_base>=80:
-		$Bruja.show()
+		$PointLight2D3/Bruja.show()
 	else:
-		$Bruja.hide()
+		$PointLight2D3/Bruja.hide()
 	pass # Replace with function body.
 	
 	if !Main.bando=="":
+		$PointLight2D/Sprite2D.show()
+	else:
+		$PointLight2D/Sprite2D.hide()
 		
 
 
