fb6457983e74761fb15b8cb1d8de6ad515e83913
Jeff <124184325+j-tor@users.noreply.github.com>
2024-03-07 23:32:42 -0700

    Segunda mesa full
    la mesa del ANALISIS termianada y la de Evidencia se arregalron problemas con los tile mapa de la U

--- New Game Project/Scene/eggcluster.gd
+++ New Game Project/Scene/eggcluster.gd
@@ -1,8 +1,8 @@
 extends Node2D
-var num = 1
+var num 
 var RespuestaCorrectaAula
 var RespuestaUserAula
-
+var fallo=0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	QuestionsLab()
@@ -15,67 +15,113 @@ func _process(delta):
 
 
 func QuestionsLab():
-	#num =randi()%6
+	num =randi()%5
 	if num==0:
-		$Control/Preguntas.text="Seleccione el mandato cuya obligación\nviene del miedo al castigo o la \nbúsqueda de un premio:
-A) Imperativo Hipotético. \nB) Imperativo categórico. \nC) Ambos. D) Ninguno"
-		RespuestaCorrectaAula="Imperativo Hipotético"
+		$Control/Preguntas.text="Son los pasos o razones de la duda metódica:\n 
+		A) Los sentidos nos engañan, B) la existencia del genio maligno, \nC) imposibilidad para diferenciar la vigilia del sueño. D) Todas son correctas "
+		RespuestaCorrectaAula="Todas son correctas"
 	elif num==1:
-		$Control/Preguntas.text="Para Emanuel Kant, es posible conocer lo \nque las cosas nos permiten (como lo \nsuperficial) a través de nuestros \nsentidos:
-A) Conocimiento Noumenico. \nB) Conocimiento fenoménico. \nC) conocimiento Empírico"
-		RespuestaCorrectaAula="Conocimiento fenoménico"
+		$Control/Preguntas.text="Una de las siguientes es considerada de las cuatro reglas del método en Descartes:\n
+		A) Hipótesis. B) Deducción. C) Evidencia. D) Inducción."
+		RespuestaCorrectaAula="Evidencia"
 	elif num==2:
-		$Control/Preguntas.text="Kant decía que el lema de la nilustración \nera “Sapere aude”, que significa:
-A) Sopesa tus acciones. \nB) Atrévete a saber por ti mismo. \nC) Saber a la fuerza.\nD) Someterse al conocimiento"
-		RespuestaCorrectaAula="Atrévete a saber por ti mismo"
+		$Control/Preguntas.text="En relación con los datos experimentales que otorgan los sentidos en el conocimiento, \nDescartes define como:\n
+		A) Los sentidos son una fuente confiable para conocer. B) Desconfianza de lo que los sentidos 
+		nos proporcionan. C) Los sentidos son complementarios. D) Los sentidos son determinantes"
+		RespuestaCorrectaAula="Desconfianza de lo que los sentidos nos proporcionan"
 	elif num==3:
-		$Control/Preguntas.text="Kant (igual que Copérnico cambió el \ncentro del universo de la tierra al sol),\ncambia el centro del conocimiento del \nobjeto al sujeto, a esto se le llama:
-A) Subjetivismo. B) Prejuicio. \nC) giro copernicano. D) Suerte"
-		RespuestaCorrectaAula="giro copernicano"
+		$Control/Preguntas.text="Solo debemos aceptar como verdadero aquel conocimiento que sea EVIDENTE, CLARO Y DISTINTO:\n 
+		A) René Descartes. B) David Hume. C) George Berkeley. D) Aristóteles"
+		RespuestaCorrectaAula="René Descartes"
 	if num==4:
-		$Control/Preguntas.text="La postura conciliadora de Kant \nrespecto a los empiristas y \nracionalistas define que los datos
-experimentales son la fuente del \nconocimiento racional del sujeto:
-A) Racionalismo. B) Empirismo. \nC) Criticismo. D) Escepticismo"
-		RespuestaCorrectaAula="Criticismo"
-	if num==5:
-		$Control/Preguntas.text="De las siguientes obras de Emanuel Kant, \nseleccione aquella que define su \nepistemología:
-A) Critica de la razón práctica. \nB) Critica de la razón pura. \nC) Critica del juicio. D) Critica
-fenomenológica"
-		RespuestaCorrectaAula="Critica de la razón pura"
-		
+		$Control/Preguntas.text="En cuanto a la certeza del conocimiento, René Descartes afirma lo siguiente:\n 
+		A) Es preciso dudar. B) Debemos confiar ciegamente. C) Nada es importante. D) Todo es posible"
+		RespuestaCorrectaAula="Es preciso dudar"
 
 
 func respuesta():
+	print(fallo)
 	if RespuestaCorrectaAula==RespuestaUserAula:
-		$Sprite2D.frame+=1
-		$Control/ProgressBar.value += 50 * 100/ 100
+		$Control/ProgressBar.value += 25 * 100/ 100
 		QuestionsLab()
-		if $Control/ProgressBar.value >= 100:
+		if $Control/ProgressBar.value >= 100 && fallo<4:
+					$CanvasModulate2/Label.text="Ganaste!!"
+					$CanvasModulate2.show()
 					$Regresar.show()
 					print("Barra llena")
+		else:
+			if fallo==2:
+				$CanvasModulate2.show()
+				$Regresar.show()
 	else:
-		$Control/ProgressBar.value -= 50 * 100/ 100
+		fallo+=1
+		if fallo==4:
+				$CanvasModulate2.show()
+				$Regresar.show()
+		QuestionsLab()
+		$Control/ProgressBar.value -= 25 * 100/ 100
 		print("Fallastes")
-	
-	
 	
 func _on_a_pressed():
 	
-	RespuestaUserAula="Conocimiento fenoménico"
+	if num==0:
+		RespuestaUserAula="Los sentidos nos engañan"
+	elif num==1:
+		RespuestaUserAula="Hipótesis"
+	elif num==2:
+		RespuestaUserAula="Los sentidos son una fuente confiable para conocer"
+	elif num==3:
+		RespuestaUserAula="René Descartes"
+	elif num==4:
+		RespuestaUserAula="Es preciso dudar"
 	respuesta()
 	
 
 
 func _on_b_pressed():
-	pass # Replace with function body.
+	
+	if num==0:
+		RespuestaUserAula="la existencia del genio maligno"
+	elif num==1:
+		RespuestaUserAula="Deducción"
+	elif num==2:
+		RespuestaUserAula="Desconfianza de lo que los sentidos nos proporcionan"
+	elif num==3:
+		RespuestaUserAula="David Hume"
+	elif num==4:
+		RespuestaUserAula="Debemos confiar ciegamente"
+	respuesta()
 
 
 func _on_c_pressed():
-	pass # Replace with function body.
+	
+	if num==0:
+		RespuestaUserAula="imposibilidad para 
+diferenciar la vigilia del sueño"
+	elif num==1:
+		RespuestaUserAula="Evidencia"
+	elif num==2:
+		RespuestaUserAula="Los sentidos son complementarios"
+	elif num==3:
+		RespuestaUserAula="George Berkeley"
+	elif num==4:
+		RespuestaUserAula="Nada es importante"
+	respuesta()
 
 
 func _on_d_pressed():
-	pass # Replace with function body.
+	if num==0:
+		RespuestaUserAula="Todas son correctas"
+	elif num==1:
+		RespuestaUserAula="Inducción"
+	elif num==2:
+		RespuestaUserAula=" Los sentidos son determinantes"
+	elif num==3:
+		RespuestaUserAula="Aristóteles"
+	elif num==4:
+		RespuestaUserAula="Todo es posible"
+	respuesta()
+
 
 
 func _on_regresar_pressed():
